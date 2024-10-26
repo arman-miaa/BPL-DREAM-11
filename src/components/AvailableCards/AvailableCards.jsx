bf7973bed590615f6190bcbff2551d563fe39bbf
Arman <mdarman365506@gmail.com>
2024-10-26 12:49:18 +0600

    come change on card gap

--- src/components/AvailableCards/AvailableCards.jsx
+++ src/components/AvailableCards/AvailableCards.jsx
@@ -5,7 +5,7 @@ export default function AvailableCards({ player, handleSelectPlayer }) {
   // console.log(player);
   return (
     <div className="container mx-auto mt-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {player.map((playerr) => (
           <AvailableCard
             key={playerr.playerId}
